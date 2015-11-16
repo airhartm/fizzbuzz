8a8847161cd818be810cbe29965c555208cbf2dd
Michael Airhart <mike@airhart.me>
2015-11-15 21:41:09 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+$( "body" ).append( "<p>" );
+
+for (counter = 1; counter<=100; counter++) {
+  if (!(counter % 3) && (counter % 5)) {
+    $( "body" ).append( "fizz<br/>" );
+    }
+  else if ((counter % 3) && !(counter % 5)) {
+    $( "body" ).append( "buzz<br/>" );
+    }
+  else if (!(counter % 3) && !(counter % 5)) {
+    $( "body" ).append( "fizzbuzz<br/>" );
+    }
+  else {
+    $( "body" ).append( counter+"<br/>" );
+    }
+}   
+    $( "body" ).append( "</p>" );
+
+})
