2f85d4e539e741bd92c8570065c47259bbe42b3a
Michael Airhart <mike@airhart.me>
2015-12-01 22:17:03 -0500

    refactored

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
 $(document).ready(function() {
 
-$( "body" ).append( "<p>" );
+//$( "body" ).append( "<p>" );
 
-for (counter = 1; counter<=100; counter++) {
-  if (!(counter % 3) && (counter % 5)) {
-    $( "body" ).append( "fizz<br/>" );
+  function fizzbuzzer (max){
+
+    for (counter = 1; counter<=max; counter++) {
+
+     if (!(counter % 3) && (counter % 5)) {
+       $( "body" ).append( "fizz<br/>" );
+        }
+     else if ((counter % 3) && !(counter % 5)) {
+       $( "body" ).append( "buzz<br/>" );
+      }
+     else if (!(counter % 3) && !(counter % 5)) {
+      $( "body" ).append( "fizzbuzz<br/>" );
+      }
+     else {
+      $( "body" ).append( counter+"<br/>" );
+      }
+
+    }   
+
+}
+
+//$( "body" ).append( "</p>" );
+var maxEntry = "Enter an integer"
+while (maxEntry!=parseInt(maxEntry) && maxEntry % 1!=0){
+    var maxEntry = prompt("Please enter an integer", maxEntry);
     }
-  else if ((counter % 3) && !(counter % 5)) {
-    $( "body" ).append( "buzz<br/>" );
-    }
-  else if (!(counter % 3) && !(counter % 5)) {
-    $( "body" ).append( "fizzbuzz<br/>" );
-    }
-  else {
-    $( "body" ).append( counter+"<br/>" );
-    }
-}   
-$( "body" ).append( "</p>" );
+fizzbuzzer(maxEntry);
 
 })
